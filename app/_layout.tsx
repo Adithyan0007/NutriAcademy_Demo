aca80fff92c27cfb01d809cc28f88b109291c711
adithyan0007 <adithyan039@gmail.com>
2026-06-22 20:18:15 +0530

    changes added in protectedroutes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,58 @@
-import { Stack } from "expo-router";
+import { Stack, router, useSegments } from "expo-router";
+import { useEffect, useState } from "react";
+import { ActivityIndicator, View } from "react-native";
+
+import { getCurrentUser } from "../data/localDb";
 
 export default function RootLayout() {
+  const segments = useSegments();
+  const [checkingAuth, setCheckingAuth] = useState(true);
+
+  useEffect(() => {
+    async function protectRoutes() {
+      const user = await getCurrentUser();
+
+      const currentRoute = segments[0];
+
+      const isPublicRoute =
+        currentRoute === undefined || currentRoute === "signup";
+
+      if (!user && !isPublicRoute) {
+        router.replace("/");
+      }
+
+      if (user && isPublicRoute) {
+        router.replace("/home");
+      }
+
+      setCheckingAuth(false);
+    }
+
+    protectRoutes();
+  }, [segments]);
+
+  if (checkingAuth) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "#F0FDF4",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <ActivityIndicator size="large" color="#166534" />
+      </View>
+    );
+  }
+
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
       <Stack.Screen name="signup" />
       <Stack.Screen name="home" />
       <Stack.Screen name="course/[id]" />
+      <Stack.Screen name="lesson/[courseId]/[lessonId]" />
       <Stack.Screen name="my-learning" />
       <Stack.Screen name="profile" />
     </Stack>
